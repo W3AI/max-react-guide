ff48fa08419031f43438d7a588e473ddf0df4b4f
stefian <v_ianta@yahoo.com>
2019-05-07 12:03:23 -0400

    added var for css classes

--- src/App.js
+++ src/App.js
@@ -73,6 +73,8 @@ class App extends Component {
             style.backgroundColor = 'red';
         }
 
+        let classes = ['red', 'bold'].join(' ');
+
         return (
             <div className="App">
                 <h1>Hey, I'm a React App</h1>
