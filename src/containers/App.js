8d2c0fad1e0b3b156c3d505c977dab4241937fe4
stefian <v_ianta@yahoo.com>
2019-05-08 09:42:19 -0400

    added constructor stub to app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        console.log('[App.js] constructor');
+    }
+
     state = {
         persons: [
             { id: 'srttfh', name: 'Max', age: 28 },
