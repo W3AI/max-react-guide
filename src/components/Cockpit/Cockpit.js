53060fc18a0c7536bc338f476cb27dbd40f3ea5d
stefian <v_ianta@yahoo.com>
2019-05-09 18:59:21 -0400

    added toggleBtnRef to auto click on Mart button when loading frst time

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import classes from './Cockpit.css';
 
 // const cockpit = ( props ) => {       // this one didn't work as per Max
 
 function Cockpit( props ) {
+    const toggleBtnRef = useRef(null);
 
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // Http request...
-        setTimeout(() => {
-            alert('Saved data to cloud!');
-        }, 1000);
+        // setTimeout(() => {
+        //     alert('Saved data to cloud!');
+        // }, 1000);
+        toggleBtnRef.current.click();
         return () => {
             console.log('[Cockpit.js] cleanup work in useEffect');
         };
@@ -48,9 +50,9 @@ function Cockpit( props ) {
                 &lt;ai team/&gt; 	&#10038;
                 &lt;ai code/&gt;
                 </p>
-            <button
+            <button ref={toggleBtnRef}
                 className={btnClass}
-                onClick={props.clicked}>Toggle Persons</button>
+                onClick={props.clicked}>Toggle skills Mart</button>
         </div>
     );
 };
