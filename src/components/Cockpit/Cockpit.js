81a43358e474a932f6882947b2629b3a49090a94
stefian <v_ianta@yahoo.com>
2019-05-09 08:04:08 -0400

    removed timer and clearTtimeout in Cockpit useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -9,11 +9,10 @@ function Cockpit( props ) {
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // Http request...
-        const timer = setTimeout(() => {
+        setTimeout(() => {
             alert('Saved data to cloud!');
         }, 1000);
         return () => {
-            clearTimeout(timer);
             console.log('[Cockpit.js] cleanup work in useEffect');
         };
     }, []);        // useEffect to execute only when [props.persons] change !!!
